9745f724132cc968865c7fa5a297bf69b7243937
llarreta <leonel@larreta.com.ar>
2021-07-10 20:11:24 -0300

    Prueba con streams

---
+++ src/main/java/ar/com/intrale/cloud/GetProductImageFunction.java
@@ -0,0 +1,29 @@
+package ar.com.intrale.cloud;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+
+import ar.com.intrale.cloud.exceptions.FunctionException;
+
+@Singleton
+@Named(GetProductImageFunction.FUNCTION_NAME)
+public class GetProductImageFunction extends BaseFunction<GetProductImageRequest, S3ObjectInputStream, AmazonS3, StringToGetProductImageRequestBuilder, ObjectToSameBuilder> {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(GetProductImageFunction.class);
+	
+	public static final String FUNCTION_NAME = "getImage";
+
+	@Override
+	public S3ObjectInputStream execute(GetProductImageRequest request) throws FunctionException {
+		LOGGER.info("GetProductImageFunction execute");
+		return provider.getObject(config.getS3().getBucketName(), request.getId()).getObjectContent();
+	}
+
+
+}
---
+++ src/main/java/ar/com/intrale/cloud/StringToGetProductImageRequestBuilder.java
@@ -0,0 +1,8 @@
+package ar.com.intrale.cloud;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class StringToGetProductImageRequestBuilder extends StringToRequestBuilder<GetProductImageRequest> {
+
+}
