e835ae937af0351bf5f7b5b05f890cd9da8451e6
llarreta <leonel@larreta.com.ar>
2021-07-10 16:05:16 -0300

    Se agregan trazas

--- src/main/java/ar/com/intrale/cloud/LambdaStream.java
+++ src/main/java/ar/com/intrale/cloud/LambdaStream.java
@@ -7,6 +7,8 @@ import java.io.OutputStream;
 import java.nio.charset.Charset;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.amazonaws.services.lambda.runtime.Context;
 
@@ -14,12 +16,17 @@ import io.micronaut.function.aws.MicronautRequestStreamHandler;
 
 public class LambdaStream extends MicronautRequestStreamHandler {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(BaseFunction.class);
+	
 	@Override
 	public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
-        Charset encoding = Charset.forName("UTF-8");
+		LOGGER.info("LambdaStream: START");        
+		Charset encoding = Charset.forName("UTF-8");
         String input = IOUtils.toString(inputStream, encoding);
+        LOGGER.info("LambdaStream: " + input);
         String output = "My first request handler: " + input;
         IOUtils.write(output, outputStream, encoding);
+        LOGGER.info("LambdaStream: FINISH");
 	}
 
 }
