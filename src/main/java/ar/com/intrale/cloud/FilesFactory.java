b4f3a3aeee4032b6d831054c85e34c3ccef702a6
llarreta <leonel@larreta.com.ar>
2021-07-10 17:20:20 -0300

    Faltaba colocar las annotations de factory

--- src/main/java/ar/com/intrale/cloud/FilesFactory.java
+++ src/main/java/ar/com/intrale/cloud/FilesFactory.java
@@ -6,8 +6,11 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 import io.micronaut.context.annotation.Bean;
+import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.annotation.Requires;
 
+@Factory
+@Requires(property = IntraleFactory.FACTORY, value = IntraleFactory.TRUE, defaultValue = IntraleFactory.TRUE)
 public class FilesFactory extends IntraleFactory<AmazonS3> {
 
 	@Bean @Requires(property = "app.instantiate.s3provider", value = IntraleFactory.TRUE, defaultValue = IntraleFactory.TRUE)
